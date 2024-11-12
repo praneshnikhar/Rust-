a90a1b8b7a50f719c2a08203324efbaf15851f67
Pranesh Nikhar <praneshnikhar@gmail.com>
2024-11-12 16:30:19 +0530

    Update main14.rs

--- main14.rs
+++ main14.rs
@@ -1,9 +1,9 @@
-fn main(){
-    let mut s1 = String::from("Hello ");
-    update_word(&mut s1);
-    println!("{}", s1);
+fn main() {
+    let mut s1 = String::from("Hello");
+    update_word(&mut s1);  // Mutable borrow to `update_word`
+    println!("{}", s1);     // Use `s1` directly here
 }
 
-fn update_word(word: &mut String){
-    word.push_str("World");
+fn update_word(word: &mut String) {
+    word.push_str(" world");
 }
