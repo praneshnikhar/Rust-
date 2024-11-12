39e2c7a5df6bba904b3002b7d7a68fb4312ad6c4
Pranesh Nikhar <praneshnikhar@gmail.com>
2024-11-12 15:47:53 +0530

    Update main12.rs

--- main12.rs
+++ main12.rs
@@ -1,7 +1,12 @@
 fn main(){
+    create_str();
+    print!("{}",2);
+}
+
+fn create_str(){
     let s1 = String::from("Hi there");
     println!("{}", s1);
     let s2= s1;
     println!("{}",s2);
-    
+
 }
