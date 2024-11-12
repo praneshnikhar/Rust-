a5a11d966311e663aba6ccd9c60cef34d3c9181e
Pranesh Nikhar <praneshnikhar@gmail.com>
2024-11-12 16:34:52 +0530

    Create main15.rs

---
+++ main15.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let my_string = String::from("Hello, Rust!");
+    takes_ownership(&my_string);  // Pass a reference to my_string
+    println!("{}", my_string);    // This is valid because ownership was not transferred
+}
+
+fn takes_ownership(some_string: &String) {
+    println!("{}", some_string);  // some_string is borrowed and not moved
+}
